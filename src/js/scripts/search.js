a78d698953bff2cff6080e31d8c0c5c2281d2937
AkmalKurbanov <250296a@gmail.com>
2022-11-03 12:36:16 +0600

    search

--- src/js/scripts/search.js
+++ src/js/scripts/search.js
@@ -3,12 +3,3 @@ $('.search-btn').on('click', function () {
 });
 
 
-jQuery(function ($) {
-  $(document).mouseup(function (e) {
-    var div = $(".search, .search-btn");
-    if (!div.is(e.target) &&
-      div.has(e.target).length === 0) {
-      $('.search').removeClass('open');
-    }
-  });
-});
